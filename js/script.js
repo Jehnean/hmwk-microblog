46bdbe10aeae7ca9345010f6935f9bd7039199c6
Jehnean <jehnean@gmail.com>
2016-01-31 20:55:47 -0800

    Adding updated files to include new JavaScript functionality with working submit, li posts, and prepending order.

--- js/script.js
+++ js/script.js
@@ -1,45 +1,39 @@
-//sanity check
-console.log("linked")
+// Checks to make sure page is loaded 
+$(document).ready(function(){
+	// Defines variables for all the items we are going to call 
+	var $blogPoster = $(".blog-poster"),
+		$form = $blogPoster.find(".blog-submit-form"),
+		$submit = $form.find("button"),
+		$blogList = $blogPoster.find(".blog-item-list ul"),
+		// Make a little close button 
+		closeButton = "<a href='#' class='close-button'>&#x2717;</a>",
+		// Make a list item, with a close button and an opening P tag
+		opl = "<li>" + closeButton + " <h2>",
+		// Close the P tag and list item 
+		cpl = "</h2><hr></li>";
 
-//wait for the page to load
-window.onload = function() {
-  //grab the btn
-  var submitButton = document.getElementById("share-btn");
-  //create an event that fires on the `submit` event
-  submitButton.addEventListener("submit", function(event) {
-    //prevent the default behavior of the form (which refreshs the page)
-    event.preventDefault();
-    console.log('HELLO!'); 
-    //log the thing the event propegated from to the console
-    console.log(event.target); 
-  });
-}; 
 
-/*
+	// On click of close button, on event
+	$blogList.on("click", ".close-button", function(e){
+		// Define this
+		var $thisLink = $(this),
+			// Go to the parent of this 
+			thisLinksParent = $thisLink.parent();
+		// and remove the parent 
+		thisLinksParent.remove();
+	});
 
-    // Get text field input input
-    var input = document.getElementById('inputField').value;
-    check(input);
-    alert('inputField'.value); 
-  });
-
-  function check (input) {
-    if (input) {
-      // Reset the textfield
-      document.getElementById('inputField').value = '';
-
-      // Create a new li object
-      var newListItem = document.createElement('li');
-
-      // Place the stored text field info into it's innerHTML
-      newListItem.innerHTML = input;
-
-      // Capture immediate child node of the ul
-      var firstChild = document.getElementById('blogPosts').children[0];
-
-      // Append first child to the new li
-      var parentNode = document.getElementById('mBlogPosts');
-      parentNode.insertBefore(newListItem, firstChild);
-    }
-  }
-};*/
+	// On form submit event
+	$form.on("submit", function(event){
+		// Prevent default behavior (new page load)
+		event.preventDefault();
+		// Define this 
+		var $thisForm = $(this),
+			// Find input 
+			$inputBlog = $thisForm.find(".input-blog-post"),
+			// Get the value from input
+			blogItem = $inputBlog.val();
+		// Get blogList and append with opl, blogItm and, cpl actions from above
+		$blogList.prepend(opl + blogItem  + cpl);
+	});
+});
